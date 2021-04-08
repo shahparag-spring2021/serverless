422c3860fb852d3164cb0cd6dc2a10d4ff20b2ea
Parag Shah <paragshah367@gmail.com>
2021-04-08 02:16:29 -0700

    Testing sending email, t2

--- lambda_function.py
+++ lambda_function.py
@@ -96,12 +96,15 @@ def lambda_handler(event, context):
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
     
 
-    {'Records': [{'EventSource': 'aws:sns', 'EventVersion': '1.0', 'EventSubscriptionArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'Sns': {'Type': 'Notification', 'MessageId': '2badc683-5337-598d-88bc-9e6bf8c3b5f1', 'TopicArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic', 'Subject': None, 'Message': 'Test message', 'Timestamp': '2021-04-08T06:59:00.999Z', 'SignatureVersion': '1', 'Signature': 'fy8/uoHSpiK8IpAkzVv8ZdfVkk6h9o05LCNfSQ6jettLlcZ4L1qrEm3c6f/HdYnfRE7Ta7XbypSWE4YpxFPZ9+RyRBKp6AGkhOkk/3+DPowNtRYuAnabaZrwfCcNdnI5GWiVxQ4xjqxKRfon7VhHXEb//e22uVxyaaoxMuMzWNEk2DcJhfeoj8BrExLiZKtZyDgYXxSbDW+0K99S8P3qwgRrcAmQiAwQLr26M1xt6KSMMwBbG/LSGvT8kwsEIAPfseMclOEf1oau9Vdq1/4Em0WP4iGcMvYdFMhQlj8CRiwFCINIzxb842Vanm1x0cAw0xH8SQKoBI6YCz7ietQdwA==', 'SigningCertUrl': 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem', 'UnsubscribeUrl': 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'MessageAttributes': {}}}]}
+    Testing Lambda {'Records': [{'EventSource': 'aws:sns', 'EventVersion': '1.0', 'EventSubscriptionArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'Sns': {'Type': 'Notification', 'MessageId': '9d1c95c1-9f03-5534-a445-cd295871b69c', 'TopicArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic', 'Subject': None, 'Message': '{"user_email": "paragshah367@gmail.com", "message": "You created a book. Book id: 66e678a6-bef2-4380-a5bb-8fa3a7258770\\n Book link: prod.paragshah.me/book/66e678a6-bef2-4380-a5bb-8fa3a7258770"}', 'Timestamp': '2021-04-08T08:56:18.349Z', 'SignatureVersion': '1', 'Signature': 'JXe9Mgym0A43AMmCm2jLiFL7aQRlUc4vON6YeuJ8sWwjq6w2Pj3GGFEwq+bGHwEqQ+otpY+IXcK+mt2n/Elr3QfPG3I/z0OELKPbVdTYPgxCuGJni2Dy1HifqUn5gf/xTH3Gk3/VV10o81PrQiNOwN2eROw2HDW7Z3exSapj1Sc8kG+tHWfKMRPE5KFnYG5N4hmD0RakMg4L+UT6i/eWDFFHSSIAKzdGdgyGKZ+zEkt8OfjvyYauZY3WL8n/wOyF0TBBIfA3BctTX5M0OMW+fZe36RALdZKTkRiKYLWMMx+m07PheZNf8JUPPJiGFlN2qjQn5CELpEOzw06R5MApOw==', 'SigningCertUrl': 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem', 'UnsubscribeUrl': 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'MessageAttributes': {}}}]}
+
+
 '''
 
 ses_client = boto3.client('ses')
 db_resource = boto3.resource('dynamodb')
-table = db_resource.Table('message_notification')
+# table = db_resource.Table('message_notification')
+table = dynamodb.Table(DYNANODB_TABLE)
 
 
 def lambda_handler(event, context):
@@ -136,7 +139,8 @@ def lambda_handler(event, context):
             }
         )
         insert_record(message)
-        return "notify the user sreuccesfully"
+        return "notify the user successfully"
+
     else:
         return "duplicate message"
 
