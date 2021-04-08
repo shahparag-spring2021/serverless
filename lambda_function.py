3d7c87b933f25ca450c0ed1858305c9ec534473e
Parag Shah <paragshah367@gmail.com>
2021-04-08 00:12:58 -0700

    Testing sending email

--- lambda_function.py
+++ lambda_function.py
@@ -96,9 +96,7 @@ def lambda_handler(event, context):
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
     
 
-    {'Records': [{'EventSource': 'aws:sns', 'EventVersion': '1.0', 'EventSubscriptionArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'Sns': {'Type': 'Notification', 'MessageId': 'f311760d-2bb8-57dd-b3be-2ee5588200d3', 'TopicArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic', 'Subject': None, 'Message': 'm1', 'Timestamp': '2021-04-08T02:56:58.948Z', 'SignatureVersion': '1', 'Signature': 'UQtKkoYpq+8wIgkRGo/XuamjWCmI6wy+4UUJ8FpI0Es/aK9uhpWb1yodjIIIUEZRlrxskRjq3NAx7xDf+psxu5IsFKaiZA2CzzrM8hdNa/mjKkJi5zPxLGaqK3X0CJ1D6rk7Ty5rbjqahy77HvMNMEbmQo/4jBKYH9sD5pBO8WW+VyTEbsO3jEGCNU+xS/8mcnACCBl6kATgWX04CEKaV5lsfl1yfXRb8pHSN6Ad1EdsJP49QD2epSo13qBu8rDvrhWi+sT02bOEo+eHNA7hswCtRrSkla45WCUBVM38tKtfCgSKLbvqpE6ErCoSylwPRHLkX/2oDVwy7gYyifyEFA==', 'SigningCertUrl': 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem', 'UnsubscribeUrl': 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'MessageAttributes': {}}
-    }
-    ]}
+    {'Records': [{'EventSource': 'aws:sns', 'EventVersion': '1.0', 'EventSubscriptionArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'Sns': {'Type': 'Notification', 'MessageId': '2badc683-5337-598d-88bc-9e6bf8c3b5f1', 'TopicArn': 'arn:aws:sns:us-east-1:578033826244:sns_topic', 'Subject': None, 'Message': 'Test message', 'Timestamp': '2021-04-08T06:59:00.999Z', 'SignatureVersion': '1', 'Signature': 'fy8/uoHSpiK8IpAkzVv8ZdfVkk6h9o05LCNfSQ6jettLlcZ4L1qrEm3c6f/HdYnfRE7Ta7XbypSWE4YpxFPZ9+RyRBKp6AGkhOkk/3+DPowNtRYuAnabaZrwfCcNdnI5GWiVxQ4xjqxKRfon7VhHXEb//e22uVxyaaoxMuMzWNEk2DcJhfeoj8BrExLiZKtZyDgYXxSbDW+0K99S8P3qwgRrcAmQiAwQLr26M1xt6KSMMwBbG/LSGvT8kwsEIAPfseMclOEf1oau9Vdq1/4Em0WP4iGcMvYdFMhQlj8CRiwFCINIzxb842Vanm1x0cAw0xH8SQKoBI6YCz7ietQdwA==', 'SigningCertUrl': 'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-010a507c1833636cd94bdb98bd93083a.pem', 'UnsubscribeUrl': 'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:578033826244:sns_topic:35ffe68f-618a-438c-af33-2287701f6b00', 'MessageAttributes': {}}}]}
 '''
 
 ses_client = boto3.client('ses')
@@ -117,6 +115,7 @@ def lambda_handler(event, context):
     status = get_record(message)
     print(status)
     if not status:
+        print('testing if not status')
         ses_response = ses_client.send_email(
             Source='parag@paragshah.me',
             Destination={
@@ -139,7 +138,7 @@ def lambda_handler(event, context):
         insert_record(message)
         return "notify the user succesfully"
     else:
-        return " duplicate message"
+        return "duplicate message"
 
 
 def insert_record(message_content):
